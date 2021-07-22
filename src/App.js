1c7b99a3c2c981b5248f95c594f3222973f47da5
brandon6190 <brandonm6190@gmail.com>
2021-07-22 16:03:10 -0400

    -Imported BrowserRouter to index.js and wrapped whole App with the Router -App: Imported Route and Switch to render page component with react router for better navigation -Directory: cleaned code by using speard operator for other props in MenuItem -MenuItem: updated with onclick handler to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const Hatspage = () => (
+  <div>Hats Page</div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Homepage />
-      </div>
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={Hatspage} />
+      </Switch>
     </div>
   );
 }
