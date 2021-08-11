b36722d1b914c67a9a68401f0c5ae8811b36f9c3
brandon6190 <brandonm6190@gmail.com>
2021-08-11 17:59:51 -0400

    -CollectionItem: created collectionItem component to display product image, name and price. -CollectionPreview: replaced div element with CollectionItem component -Created a style sheet for CollectionItem

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => (
@@ -8,8 +10,8 @@ const CollectionPreview = ({title, items}) => (
         <div className="preview">
             {items
                 .filter((item, idx) => idx < 4) //* Preview only 4 items
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps}/>
             ))}
         </div>
     </div>
